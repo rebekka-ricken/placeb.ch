8602d3451d607a7183a3afead7c561ad5d6d5ade
maersu <me@maersu.ch>
2016-02-29 21:37:41 +0100

    dedect phone

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -2,9 +2,32 @@ window.intercomSettings = {
     app_id: "upu5b2be"
 };
 
+var isMobile = {
+    Android: function () {
+        return /Android/i.test(navigator.userAgent);
+    },
+    iOS: function () {
+        return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+    }
+};
+
 $(document).ready(function () {
     $('.open-chat').on('click', function () {
         Intercom('showNewMessage');
+        return false;
+    });
+
+    $('.download-app').on('click', function () {
+
+        if (isMobile.iOS()) {
+            window.location.href = "https://itunes.apple.com/us/app/placeb/id1073601192";
+        }
+        else if (isMobile.Android()) {
+            window.location.href = "https://play.google.com/store/apps/details?id=com.kg.placeb";
+        } else {
+            $('html, body').animate({scrollTop: $('#mobile-app').offset().top - 20});
+        }
+
         return false;
     });
 
