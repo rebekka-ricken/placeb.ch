f52a305176a5bffdcc0cddc197db05a2f0a4eb68
maersu <me@maersu.ch>
2016-02-29 14:53:51 +0100

    remove js snippet

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -8,10 +8,3 @@ $(document).ready(function () {
         return false;
     })
 });
-
-(function (window, document) {
-    document.getElementById('toggle').addEventListener('click', function (e) {
-        document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
-        document.getElementById('toggle').classList.toggle('x');
-    });
-})(this, this.document);
