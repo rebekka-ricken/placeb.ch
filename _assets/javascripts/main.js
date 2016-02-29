f3dc05a1a501a6c68ebc387ca5660c415e93b7f0
maersu <me@maersu.ch>
2016-02-29 17:50:57 +0100

    new visiuals, scroll to heading

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -6,5 +6,12 @@ $(document).ready(function () {
     $('.open-chat').on('click', function () {
         Intercom('showNewMessage');
         return false;
-    })
+    });
+
+    var hash = window.location.hash;
+
+    if (hash) {
+        $('html, body').animate({scrollTop: $(hash).offset().top - 20});
+    }
+
 });
