fe5592dd3b2d5e0cfea58c855c1b560759b39051
maersu <me@maersu.ch>
2016-03-01 03:14:05 +0100

    proto type

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -11,6 +11,106 @@ var isMobile = {
     }
 };
 
+var s_boxes = [
+    {
+        "name": "duschkabine",
+        "price": "ab CHF 39.-/Monat",
+        "img": "/assets/boxes/1_duschkabine.jpg"
+    },
+    {
+        "name": "telekabine",
+        "price": "ab CHF 39.-/Monat",
+        "img": "/assets/boxes/3_telekabine.jpg"
+    },
+    {
+        "name": "gaste_wc",
+        "price": "ab CHF 39.-/Monat",
+        "img": "/assets/boxes/6_gaste_wc.jpg"
+    }
+];
+
+var m_boxes = [
+    {
+        "name": "9_kleinbus",
+        "price": "ab CHF 39.-/Monat",
+        "img": "/assets/boxes/9_kleinbus.jpg"
+    },
+    {
+        "name": "12_kleiner_keller",
+        "price": "ab CHF 39.-/Monat",
+        "img": "/assets/boxes/12_kleiner_keller.jpg"
+    },
+    {
+        "name": "15_transporter",
+        "price": "ab CHF 39.-/Monat",
+        "img": "/assets/boxes/15_transporter.jpg"
+    }
+];
+
+var l_boxes = [
+    {
+        "name": "18_einzelgarage",
+        "price": "ab CHF 39.-/Monat",
+        "img": "/assets/boxes/18_einzelgarage.jpg"
+    },
+    {
+        "name": "22_kinderzimmer",
+        "price": "ab CHF 39.-/Monat",
+        "img": "/assets/boxes/22_kinderzimmer.jpg"
+    },
+    {
+        "name": "23_doppelburo",
+        "price": "ab CHF 39.-/Monat",
+        "img": "/assets/boxes/23_doppelburo.jpg"
+    }
+];
+
+var xl_boxes = [
+    {
+        "name": "30_wohnzimmer",
+        "price": "ab CHF 39.-/Monat",
+        "img": "/assets/boxes/30_wohnzimmer.jpg"
+    },
+    {
+        "name": "26_elternschlafzimmer",
+        "price": "ab CHF 39.-/Monat",
+        "img": "/assets/boxes/26_elternschlafzimmer.jpg"
+    }
+]
+
+
+var flipper = {
+
+    start: function (size, duration, delay, boxes) {
+
+        var current = 1;
+        var site = 'bg';
+
+        var flip = function() {
+            var selector = '#' + size + '-' + site;
+            var box = boxes[current];
+            $(selector + "-img").attr('src', box["img"]);
+            $(selector + "-name").html(box["name"]);
+            $(selector + "-price").html(box["price"]);
+            current += 1;
+            setTimeout(function () {
+                $('#' + size + '-flipper').toggleClass('hover');
+            }, 1000);
+
+            if (current >= boxes.length) {
+                current = 0;
+            }
+            site = (site == 'fr') ? 'bg' : 'fr';
+        };
+
+        setTimeout(function () {
+            flip();
+            setInterval(flip, duration);
+        }, delay);
+    }
+
+}
+
 $(document).ready(function () {
     $('.open-chat').on('click', function () {
         Intercom('showNewMessage');
@@ -31,11 +131,15 @@ $(document).ready(function () {
         return false;
     });
 
+    flipper.start('s', 6000, 0, s_boxes);
+    flipper.start('m', 6000, 2400, m_boxes);
+    flipper.start('l', 6000, 3800, l_boxes);
+    flipper.start('xl', 6000, 5100, xl_boxes);
+
     $('.testimonials').slick({
         slidesToShow: 3,
         slidesToScroll: 2,
-        responsive: [{
-        }, {
+        responsive: [{}, {
             breakpoint: 600,
             settings: {
                 slidesToShow: 2,
